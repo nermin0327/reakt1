29aeb2b84b98ca2f49b069464a6aab1a3a99ac5d
nermin0327 <114156272+nermin0327@users.noreply.github.com>
2022-11-22 14:33:51 +0400

    shabhsdvhja

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import  React from "react"
+import './index.css'
+const App=()=>{
+ return(
+  <> 
+  <div className="div>">
+  <h3 className="name">Nermin Seyidehmedova</h3>
+  <p className="text"> now i can create component</p>
+  </div>
+  </>
+ )
+   
+ }
+ 
+ export default App
